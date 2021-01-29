eb2346d1974eaa5b45f7afb00d21cfbd8eded73b
Ismail Sacic <>
2021-01-29 16:22:00 +0100

    Request
    Requests to join the  server is DONE! Now you can almost join each other. We need to do one more thing before everything is in done. We need to make a socket.io server!

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify,request
 import json
 from random import randrange
 from alla_funktioner import *
@@ -12,9 +12,17 @@ def home():
 def offline_tictactoe():
     return render_template("offline_tictactoe.html")
 
-@app.route('/join')
+@app.route('/join',methods = ['POST', 'GET'])
 def join():
-    return "Join"
+    #Sparar informationen från requestst i code variablen
+    code = request.form['data']
+    #Sprar koden med storbokstav i variablen code!
+    code = code.upper()
+    
+    if len(code) == 4:
+        return jsonify({"success":"Joining..."})
+    else:
+        return jsonify({"error":"The server code must contain 4 characters"})
 
 
 @app.route('/host')
